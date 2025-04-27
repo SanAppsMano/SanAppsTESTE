37eef618df149b0e043ba9b86a918206ec11800a
SanAppsMano <manosandeslimafilho@gmail.com>
2025-04-27 12:25:49 -0300

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,8 @@
+// api/search.js
+import axios from "axios";
+export default async function handler(req, res) {
+  const termo = req.query.q;
+  // … resto da lógica …
+  res.status(200).json({ /* resultado */ });
+}
 
