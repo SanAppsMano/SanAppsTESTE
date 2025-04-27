c5101abafb82733643d0b9679e3dca53e6aa3f83
SanAppsMano <manosandeslimafilho@gmail.com>
2025-04-27 12:25:15 -0300

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,7 @@
-
-// functions/hello.js
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Olá!' })
-  }
+// api/hello.js
+export default async function handler(req, res) {
+  res.status(200).json({
+    message: "Olá da Vercel Function!",
+    now: new Date().toISOString(),
+  });
 }
