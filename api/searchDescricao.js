5b8ec0683a944d27d2b493fd7f05e54795479a8e
SanAppsMano <manosandeslimafilho@gmail.com>
2025-04-27 12:26:06 -0300

    Update searchDescricao.js

--- api/searchDescricao.js
+++ api/searchDescricao.js
@@ -1,1 +1,8 @@
 
+// api/searchDescricao.js
+import axios from "axios";
+export default async function handler(req, res) {
+  const desc = req.query.desc;
+  // … resto da lógica …
+  res.status(200).json(/* dados */);
+}
